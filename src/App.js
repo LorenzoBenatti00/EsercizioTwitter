9e4b7d1ef075a2c7fa924844fe03c554467dc4ef
LorenzoBenatti00 <99197848+LorenzoBenatti00@users.noreply.github.com>
2022-12-06 10:07:11 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const tweet= [
+    {
+      id: 55,
+      image:'https://i0.wp.com/tecnicafotografica.net/wp-content/uploads/2020/05/zajdgnxsmeg-e1590956555206.jpg?resize=768%2C432&ssl=1',
+      user: "Lorenzo Benatti",
+      tweetText: "testo Tweet",
+      tweetDate: new Date(),
+      likes: 20, 
+    },
+    {
+      id: 56,
+      image:'https://i0.wp.com/tecnicafotografica.net/wp-content/uploads/2020/05/zajdgnxsmeg-e1590956555206.jpg?resize=768%2C432&ssl=1',
+      user: "Lorenzo Benatti",
+      tweetText: "testo Tweet",
+      tweetDate: new Date(),
+      likes: 20,
+    },
+    {
+      id: 57,
+      image:'https://i0.wp.com/tecnicafotografica.net/wp-content/uploads/2020/05/zajdgnxsmeg-e1590956555206.jpg?resize=768%2C432&ssl=1',
+      user: "Lorenzo Benatti",
+      tweetText: "testo Tweet",
+      tweetDate: new Date(),
+      likes: 20,
+    },
+  ];
+  
+ return (
+  <Tweet />
+ );
 }
 
 export default App;
